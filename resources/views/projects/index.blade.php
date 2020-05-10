a07d07aab0e246ce3b318bc2a7a1406fda59649d
cleveland <clevelandmarcus@hotmail.co.uk>
2020-05-10 20:43:17 +0100

    add tailwindccss

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -2,12 +2,12 @@
 
 @section('content')
     <div style="display: flex; align-items: center">
-    <h1 style="margin-right: auto">Projects:</h1>
+    <h1 class="card-header" style="margin-right: auto">Projects:</h1>
     <a href="/projects/create">New Project</a>
     </div>
         <ul>
         @forelse($projects as $project)
-            <li>
+            <li class="list-decimal">
                 <a href="{{$project->path()}}">{{$project->title}}</a>
             </li>
 
